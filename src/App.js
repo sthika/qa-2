31d7525bc1e19d8bc91157aa5ca5b7c871d61ccc
sthika <sveta.tyo45@gmail.com>
2023-09-27 17:20:20 +0600

    published at https://sthika.github.io/qa-2/

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Home from './pages/Home';
-import About from './pages/About';
 import Collection from './pages/Collection';
 import Footer from './components/Footer';
 import Contact from './components/contact/Contact';
@@ -12,7 +11,7 @@ import Blog from './pages/Blog/Blog';
 function App() {
   return (
     <div className='App'>
-      <Router>
+      <Router basename={'/qa-2'}>
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />

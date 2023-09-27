9ce4433cf37b6e4b0f2c660c9f0f1862f81092f3
sthika <sveta.tyo45@gmail.com>
2023-09-27 21:13:03 +0600

    fixed media

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import { NavLink } from 'react-router-dom';
 import Button from '../Button';
 // import Input from '../Input';
 import HeaderMobile from '../header/HeaderMobile';
 import Contact from '../contact/Contact';
+import LoginForm from '../loginform/Login';
 
 const Header = () => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [loginBtnIsActive, setLoginBtnIsActive] = useState(false);
+
+  const setLoginBtnState = useCallback(() => {
+    setLoginBtnIsActive(!loginBtnIsActive);
+    
+  }, [loginBtnIsActive]);
+
+  console.log(loginBtnIsActive)
+  
+  const handleSubmitLogin = async (e) => {
+    e.preventDefault();
+      setIsAuth(true);
+      console.log('auth true');
+      setLoginBtnState();
+    }
+  
+
   return (
     <header className='header'>
       <div className='container header__wrapper'>
@@ -26,7 +45,16 @@ const Header = () => {
             Contact
           </NavLink>
 
-          <Button text={'Log in'} />
+          {isAuth ? (
+            <p>hi, a</p>
+          ) : (
+            <Button text={'Log in'} className={'header__sign-up'} onClick={()=>{setLoginBtnIsActive(!loginBtnIsActive)}} />
+          )}
+          <LoginForm
+            handleSubmitLogin={handleSubmitLogin}
+            loginBtnIsActive={loginBtnIsActive}
+            setLoginBtnState={setLoginBtnState}
+          />
         </nav>
 
         <HeaderMobile />
